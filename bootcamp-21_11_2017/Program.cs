a5df8750b52cf23e0b3d8abc9b3c90139f0e1bf7
unknown <mpapazekos@gmail.com>
2017-11-21 15:47:44 +0200

    Added Utilities exercise

---
+++ bootcamp-21_11_2017/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bootcamp_21_11_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            int num2 = 5;
+
+            Console.WriteLine($"num1: {num1} , num2: {num2}");
+
+            Utilities.Swap(ref num1,ref num2);
+            Console.WriteLine($"num1: {num1} , num2: {num2}");
+
+            string d1 = "left";
+            string d2 = "right";
+            Console.WriteLine($"d1: {d1} , d2: {d2}");
+
+            Utilities.Swap(ref d1, ref d2);
+            Console.WriteLine($"d1: {d1} , d2: {d2}");
+
+            Console.WriteLine("-----------------");
+            List<int> list1 = new List<int> { 5, 2, 2, 2, 6, 6, 2, 2, 4, 4, 6, 6, 6, 6, 6, 6, 7,7,4,7,7,7,7,7, 2, 4, 10, 10, 10, 10, 10 };
+            Utilities.PrintList(list1);
+
+            List<int> newList1 = Utilities.SubSeq(list1);
+            Utilities.PrintList(newList1);
+
+            Console.WriteLine("-----------------");
+            List<string> list2 = new List<string>
+            { "game",
+              "set",
+              "match",
+              "park",
+              "park",
+              "park",
+              "get",
+              "a","a","b","a","a","a"
+            };
+            Utilities.PrintList(list2);
+
+            List<string> newList2 = list2.SubSeq() ;
+            Utilities.PrintList(newList2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bootcamp-21_11_2017/Utilities.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bootcamp_21_11_2017
+{
+    public static class Utilities
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Swap<T>(ref T a,ref T b)
+        {
+            T temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static List<int> SubSeq(this List<int> theList)
+        {
+            int max = 1;
+            int pos = 0;
+
+            for (int i = 0; i < theList.Count - 1; i++)
+            {
+                int tempMax = 1;
+
+                while (theList[i + 1] == theList[i])
+                {
+                    tempMax++;
+                    i++;
+
+                    if(i == theList.Count - 1)
+                    {
+                        break;
+                    }
+                }
+
+                if (tempMax > max)
+                {
+                    max = tempMax;
+                    pos = i;
+                }            
+            }
+
+            List<int> results = new List<int>();
+
+            while (max > 0)
+            {
+                results.Add(theList[pos]);
+                max--;
+            }
+
+            return results;
+        }
+
+
+        public static List<T> SubSeq<T>(this List<T> theList)
+        {
+            int max = 1;
+            int pos = 0;
+
+            for (int i = 0; i < theList.Count - 1; i++)
+            {
+                int tempMax = 1;
+
+                while (theList[i + 1].Equals(theList[i]))
+                {
+                    tempMax++;
+                    i++;
+
+                    if (i == theList.Count - 1)
+                    {
+                        break;
+                    }
+                }
+
+                if (tempMax > max)
+                {
+                    max = tempMax;
+                    pos = i;
+                }
+            }
+
+            List<T> results = new List<T>();
+
+            while (max > 0)
+            {
+                results.Add(theList[pos]);
+                max--;
+            }
+
+            return results;
+        }
+
+
+        public static void PrintList<T>(List<T> theList)
+        {
+            foreach(var item in theList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\n-----------------");
+        }
+        
+    }
+}
